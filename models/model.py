bbaf44d6dd03ea1f763511cc8e45ce0cfe131ba3
Coldrain7 <2057787038@qq.com>
2025-10-21 16:44:16 +0800

    MySTG初版，使用了随机初始化的分组矩阵，只使用一个学习组件关系的transformer

--- models/model.py
+++ models/model.py
@@ -1,6 +1,8 @@
+import math
 import torch
 import torch.nn as nn
 from timm.models.vision_transformer import Attention, Mlp
+import torch.nn.functional as F
 
 class WindowAttBlock(nn.Module):
     def __init__(self, hidden_size, num_heads, num, size, mlp_ratio=4.0):
@@ -29,12 +31,12 @@ class WindowAttBlock(nn.Module):
         qkv = self.snorm1(x.reshape(B*T*P,N,D))
         x = x + self.sattn(qkv).reshape(B,T,P,N,D)
         x = x + self.smlp(self.snorm2(x))
-        
+
         # breadth attention
         qkv = self.nnorm1(x.transpose(2,3).reshape(B*T*N,P,D))
         x = x + self.nattn(qkv).reshape(B,T,N,P,D).transpose(2,3)
         x = x + self.nmlp(self.nnorm2(x))
-         
+
         return x.reshape(B,T,-1,D)
 
 class PatchSTG(nn.Module):
@@ -73,6 +75,7 @@ class PatchSTG(nn.Module):
         self.spa_encoder = nn.ModuleList([
             WindowAttBlock(dims, 1, spa_patchnum//factors, spa_patchsize*factors, mlp_ratio=1) for _ in range(layers)
         ])
+        print(f'spa_patchnum//factors:{spa_patchnum//factors}, spa_patchsize*factors{spa_patchsize*factors}')
 
         # projection decoder -> section 4.4 in paper
         self.regression_conv = nn.Conv2d(in_channels=tem_patchnum*dims, out_channels=output_len, kernel_size=(1, 1), bias=True)
@@ -93,6 +96,7 @@ class PatchSTG(nn.Module):
         orginal[:,:,self.ori_parts_idx,:] = rex[:,:,self.reo_parts_idx,:] # back to the original indices
 
         # projection decoder -> section 4.4 in paper
+        print(f'orginal.transpose(2,3).reshape(orginal.shape[0],-1,orginal.shape[-2],1) shape:{orginal.transpose(2,3).reshape(orginal.shape[0],-1,orginal.shape[-2],1).shape}')
         pred_y = self.regression_conv(orginal.transpose(2,3).reshape(orginal.shape[0],-1,orginal.shape[-2],1))
 
         return pred_y # [B,T,N,1]
@@ -103,7 +107,119 @@ class PatchSTG(nn.Module):
         # input traffic + time of day + day of week as the input signal
         x1 = torch.cat([x,(te[...,0:1]/self.tod),(te[...,1:2]/self.dow)], -1).float()
         input_data = self.input_st_fc(x1.transpose(1,3)).transpose(1,3)
-        t, d = input_data.shape[1], input_data.shape[-1]        
+        t, d = input_data.shape[1], input_data.shape[-1]
+
+        # cat time of day embedding
+        t_i_d_data = te[:, -input_data.shape[1]:, :, 0]
+        input_data = torch.cat([input_data, self.time_in_day_emb[(t_i_d_data).type(torch.LongTensor)]], -1)
+
+        # cat day of week embedding
+        d_i_w_data = te[:, -input_data.shape[1]:, :, 1]
+        input_data = torch.cat([input_data, self.day_in_week_emb[(d_i_w_data).type(torch.LongTensor)]], -1)
+
+        # cat spatial embedding
+        node_emb = self.node_emb.unsqueeze(0).unsqueeze(1).expand(b, t, -1, -1)
+        input_data = torch.cat([input_data, node_emb], -1)
+
+        return input_data
+class MySTG(nn.Module):
+    def __init__(self, output_len, tem_patchsize, tem_patchnum,
+                        node_num, group_size, group_num,
+                        tod, dow,
+                        layers,
+                        input_dims, node_dims, tod_dims, dow_dims, dropout, ff_dim
+                ):
+        super(MySTG, self).__init__()
+        self.node_num = node_num
+        self.tod, self.dow = tod, dow
+        self.group_size = group_size
+
+        # model_dims = input_emb + spa_emb + tem_emb
+        dims = input_dims + tod_dims + dow_dims + node_dims
+
+        # spatio-temporal embedding -> section 4.1 in paper
+        # input_emb
+        self.input_st_fc = nn.Conv2d(in_channels=3, out_channels=input_dims, kernel_size=(1, tem_patchsize), stride=(1, tem_patchsize), bias=True)
+        # spa_emb
+        self.node_emb = nn.Parameter(
+                torch.empty(node_num, node_dims))
+        nn.init.xavier_uniform_(self.node_emb)
+        # tem_emb
+        self.time_in_day_emb = nn.Parameter(
+                torch.empty(tod, tod_dims))
+        nn.init.xavier_uniform_(self.time_in_day_emb)
+        self.day_in_week_emb = nn.Parameter(
+                torch.empty(dow, dow_dims))
+        nn.init.xavier_uniform_(self.day_in_week_emb)
+
+        self.group_transformer = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(dims, nhead=4, dim_feedforward=ff_dim, dropout=dropout, batch_first=True),
+            layers
+        )
+        self.member_transformer = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(dims, nhead=4, dim_feedforward=ff_dim, dropout=dropout, batch_first=True),
+            layers)
+        # projection decoder -> section 4.4 in paper
+        self.regression = nn.Linear(dims*2, dims)
+        self.regression_conv = nn.Conv2d(in_channels=tem_patchnum * dims, out_channels=output_len, kernel_size=(1, 1),
+                                         bias=True)
+        self.group_matrix = nn.Parameter(torch.empty(node_num, group_num))
+        self.group_learner = nn.Sequential(
+            nn.Linear(dims, 64),
+            nn.ReLU(),
+            nn.Linear(64, group_num)
+        )
+        nn.init.kaiming_uniform_(self.group_matrix, a=math.sqrt(5))
+        # nn.init.xavier_normal_(self.group_matrix)
+
+    def forward(self, x, te):
+        # x: [B,T,N,1] input traffic
+        # te: [B,T,N,2] time information
+
+        # spatio-temporal embedding -> section 4.1 in paper
+        embedded_x = self.embedding(x, te)
+        batch_size, _, num_nodes, dim = embedded_x.shape
+        # embedded_x: [B,1,N,D] input traffic
+        # dynamic_weights = self.group_learner(embedded_x.squeeze(1))  # [B, N, g]
+        G = F.softmax(self.group_matrix, dim=0)
+        # group_x = torch.einsum('bgn,bnd->bgd', G.transpose(1,2), embedded_x.squeeze(1))
+        group_x = G.transpose(0,1) @ embedded_x
+        group_out = self.group_transformer(group_x.squeeze(1))
+        group_out = G @ group_out
+        # group_out = torch.einsum('bng,bgd->bnd', G, group_out)
+        group_out = group_out.transpose(1,2).unsqueeze(-1)
+
+        # group_matrix = self.group_matrix.unsqueeze(0).expand(batch_size, -1, -1)  # (B, N, g)
+
+        # topk_values, indices = torch.topk(group_matrix, k=self.group_size, dim=1)  # indices.shape: (B, s, g)
+        # indices = indices.permute(0, 2, 1)  # shape: (B, g, s)
+
+        # 简洁方法：直接创建批次索引
+        # batch_indices = torch.arange(batch_size, device=embedded_x.device)
+        # batch_indices = batch_indices.view(batch_size, 1, 1).expand(-1, indices.size(1), indices.size(2))  # (B, g, s)
+
+        # 直接索引，不需要扩展特征维度
+        # selected_features = group_out.squeeze(1)[batch_indices, indices]  # (B, g, s, D)
+
+        # 重新reshape为 (B*g, s, D)
+        #group_features = selected_features.reshape(-1, self.group_size, dim)
+        #member_out = self.member_transformer(group_features).reshape(batch_size, -1, dim).transpose(1,2).unsqueeze(-1)
+
+        # out = torch.cat([member_out, group_out], dim=-1)
+        # out = self.regression(out).transpose(1,2).unsqueeze(-1)
+        # projection decoder -> section 4.4 in paper
+        # out(batch_size, D, N, 1)
+        pred_y = self.regression_conv(group_out)
+
+        return pred_y # [B,T,N,1]
+
+    def embedding(self, x, te):
+        b,t,n,_ = x.shape
+
+        # input traffic + time of day + day of week as the input signal
+        x1 = torch.cat([x,(te[...,0:1]/self.tod),(te[...,1:2]/self.dow)], -1).float()
+        input_data = self.input_st_fc(x1.transpose(1,3)).transpose(1,3)
+        t, d = input_data.shape[1], input_data.shape[-1]
 
         # cat time of day embedding
         t_i_d_data = te[:, -input_data.shape[1]:, :, 0]
